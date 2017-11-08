493a6e3444d80a7600a2fc3f764f05fa98138a4c
Nathan Porter <nate@goldenstatefruit.com>
2017-11-08 13:11:40 -0800

    Fix for PHP7 substr(…) behavior in Controller::referer(…)

--- cake/libs/controller/controller.php
+++ cake/libs/controller/controller.php
@@ -926,8 +926,8 @@ class Controller extends Object {
 			$base = FULL_BASE_URL . $this->webroot;
 			if (strpos($ref, $base) === 0) {
 				$return =  substr($ref, strlen($base));
-				if ($return[0] != '/') {
-					$return = '/'.$return;
+				if (!$return or $return[0] != '/') {
+					$return = '/' . $return;
 				}
 				return $return;
 			} elseif (!$local) {
