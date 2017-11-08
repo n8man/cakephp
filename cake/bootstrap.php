16b364b86c07ffc5dfe5e4ebae695d983f8d1157
Nathan Porter <nate@goldenstatefruit.com>
2017-11-08 13:00:06 -0800

    Suppress ‘Declaration of’ warnings =(

--- cake/bootstrap.php
+++ cake/bootstrap.php
@@ -37,3 +37,9 @@ require LIBS . 'set.php';
 require LIBS . 'cache.php';
 Configure::getInstance();
 require CAKE . 'dispatcher.php';
+
+if (PHP_MAJOR_VERSION >= 7) {
+	set_error_handler(function ($errno, $errstr, $file) {
+		return (strpos($file, '/cake/') !== false && strpos($errstr, 'Declaration of') === 0);
+	}, E_WARNING);
+}
